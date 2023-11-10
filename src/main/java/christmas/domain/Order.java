4cb339b8dc954c74d0b992436ab9be77f2455a80
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-11-10 16:02:29 +0900

    fix: 음료만 주문 시, 예외 처리

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,13 +1,33 @@
 package christmas.domain;
 
+import christmas.domain.menus.Drink;
 import christmas.domain.menus.Menus;
 import java.util.Map;
 
 public class Order {
+    private static final String INVALID_ORDER_FORMAT_ERROR = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
     private final Map<Menus, Integer> orders;
 
-    public Order(Map<Menus, Integer> orders) {
-        this.orders = orders;
+    public Order(Map<Menus, Integer> input) {
+        validate(input);
+        this.orders = input;
+    }
+
+    private void validate(Map<Menus, Integer> input) {
+        validateOnlyDrink(input);
+    }
+
+    private void validateOnlyDrink(Map<Menus, Integer> input) {
+        for (Menus key : input.keySet()) {
+            if (!isInstanceOf(key)) {
+                return;
+            }
+        }
+        throw new IllegalArgumentException(INVALID_ORDER_FORMAT_ERROR);
+    }
+
+    private static boolean isInstanceOf(Menus key) {
+        return key instanceof Drink;
     }
 
     public Map<Menus, Integer> getOrders() {

e5df7f3306e502876f40a91ba51f6c403351ce67
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-11-10 19:17:38 +0900

    fix: 최대 주문 개수 제한 처리

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 public class Order {
     private static final String INVALID_ORDER_FORMAT_ERROR = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private static final int MAX_ORDER_COUNT = 20;
     private final Map<Menus, Integer> orders;
 
     public Order(Map<Menus, Integer> input) {
@@ -15,6 +16,21 @@ public class Order {
 
     private void validate(Map<Menus, Integer> input) {
         validateOnlyDrink(input);
+        validateMaxOrderCount(input);
+    }
+
+    private void validateMaxOrderCount(Map<Menus, Integer> input) {
+        int count = 0;
+        for (Menus key : input.keySet()) {
+            count += input.get(key);
+        }
+        if (exceedMaxCount(count)) {
+            throw new IllegalArgumentException(INVALID_ORDER_FORMAT_ERROR);
+        }
+    }
+
+    private static boolean exceedMaxCount(int count) {
+        return count > MAX_ORDER_COUNT;
     }
 
     private void validateOnlyDrink(Map<Menus, Integer> input) {
